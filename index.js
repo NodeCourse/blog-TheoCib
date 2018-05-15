109d9c2dc4facddc1c72005f73e20c99d1e8be05
TheoCib <theophile.cibert@ynov.com>
2018-05-15 12:51:47 +0200

    BDD ok

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const express = require('express');
+const Sequelize  = require('sequelize');
 const app = express();
 app.set('view engine', 'pug');
 app.use(express.static("public"));
 
-app.get('/',(request, response) => {
+/*app.get('/',(request, response) => {
     response.render("home");
+});*/
+
+const db = new Sequelize('userTest', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
 });
+
+const User = db.define('user', {
+    fullname: { type: Sequelize.STRING },
+    email: { type: Sequelize.STRING }
+});
+
+User
+    .sync()
+    .then(() => {
+        User.create({
+            fullname: 'Jane Doe',
+            email: 'jane.doe@gmail.com'
+        });
+    })
+    .then(() => {
+        User.create({
+            fullname: 'John Doe',
+            email: 'john.doe@gmail.com'
+        });
+    })
+    .then(() => {
+        return User.findAll();
+    })
+    .then((users) => {
+        console.log(users);
+    });
 
 app.listen(3000);
