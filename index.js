ba02852d159be7c3c2dfcb2ea3527b93366d674b
TheoCib <theophile.cibert@ynov.com>
2018-05-16 15:01:19 +0200

    addUser succesful

--- index.js
+++ index.js
@@ -1,28 +1,58 @@
 const express = require('express');
 const Sequelize  = require('sequelize');
+const bodyParser = require('body-parser');
 const app = express();
 app.set('view engine', 'pug');
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true})); //parser le body a partir du form (telecharcher npm install body-parser --save
 
+/*
 app.get('/',(request, response) => {
 Post
     .findAll({include: [Vote]})
     .then(posts => response.render("home", {posts}));
 
 });
-
-//app.use(bodyParser.urlencoded({extended: true})); parser le body a partir du form (telecharcher npm install body-parser --save
-
-const db = new Sequelize('userTest', 'root', '', {
+*/
+const db = new Sequelize('user', 'root', '', {
     host: 'localhost',
     dialect: 'mysql'
 });
 
 const User = db.define('user', {
-    fullname: { type: Sequelize.STRING },
-    email: { type: Sequelize.STRING }
+    firstname: { type: Sequelize.STRING },
+    lastname: { type: Sequelize.STRING },
+    email: { type: Sequelize.STRING },
+    password: { type: Sequelize.STRING }
 });
 
+app.get('/', (req,res) => {
+   res.render('home')
+});
+
+app.get('/adduser', (req,res) => {
+    res.render('adduser')
+});
+
+app.post('/adduser', (req,res) => {
+
+    User
+        .sync()
+        .then(() => {
+            User.create({
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            email: req.body.email,
+            password: req.body.password
+                }
+            )
+        })
+        .then(()=>{
+            res.redirect('/')
+        })
+});
+
+/*
 User
     .sync()
     .then(() => {
@@ -43,13 +73,13 @@ User
     .then((users) => {
         console.log(users);
     });
-
+*/
 const Post = db.define('post', {
     title:{
         type: Sequelize.STRING
     }
 });
-
+/*
 Post
     .sync()
     .then(()=> {
@@ -58,6 +88,7 @@ Post
         })
 
 });
+
 
 const Vote = db.define('vote', {
     action: {
@@ -76,8 +107,7 @@ app.post('/api/post/:postId/upvote', (req,res) =>{
         .create({action: 'up', postId: req.params.postId})
         .then (() => res.redirect('/'))
 });
-
+*/
 app.listen(3000);
 
 
-//test
